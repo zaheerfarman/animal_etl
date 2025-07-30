ef496a9cc33ea7fa46ca2bc03345b016ee3b6366
Zaheer Farman <zaheerfarman50@gmail.com>
2025-07-30 15:15:09 +0500

    Fix: E302 and E501

--- animal_etl/utils.py
+++ animal_etl/utils.py
@@ -2,8 +2,14 @@ from dateutil import parser
 from datetime import timezone
 from typing import Dict
 
+
 def transform_animal(animal: Dict) -> Dict:
-    animal['friends'] = [f.strip() for f in animal.get('friends', '').split(',') if f.strip()]
+    animal['friends'] = [
+        f.strip()
+        for f in animal.get('friends', '').split(',')
+        if f.strip()
+    ]
+
     if animal.get('born_at'):
         try:
             dt = parser.parse(animal['born_at']).astimezone(timezone.utc)
